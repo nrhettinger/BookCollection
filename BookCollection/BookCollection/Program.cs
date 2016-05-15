3beb5174a6068962e1a80cfe977c1efcb0c11d2f
Nick <nrhettinger@gmail.com>
2016-05-15 12:17:28 -0400

    completed the addNewBook method

--- BookCollection/BookCollection/Program.cs
+++ BookCollection/BookCollection/Program.cs
@@ -13,28 +13,39 @@ namespace BookCollection
             Console.WriteLine("Welcome to the book collection, what would you like to do?");
             string Response = Console.ReadLine();
             if (Response == "Create a new book") {
-                addNewBook();
-            }
-        }
-    }
+                Book newBook = new Book();
+                addNewBook(Book newBook) {
 
-    class Book
+                }
+        } 
+      
+ 
+    
+
+
+    public class Book
     {
         public string Title { get; set; }
         public string Series { get; set; }
-        public int MyProperty { get; set; }
         public int ISBN { get; set; }
         public string Author { get; set; }
         public string Review { get; set; }
-        
-        public void addNewBook()
-        {
-            Book newBook = new Book();
 
-        }
-        createNewBook
+        
+
+        public static Book addNewBook (Book newBook)
         {
-          
+                Console.WriteLine("What is the name of the book?");
+                newBook.Title = Console.ReadLine();
+                Console.WriteLine("What series is this book part of? If not part of any, say 'None'.");
+                newBook.Series = Console.ReadLine();
+                Console.WriteLine("What is the ISBN? If unknown say 'N/A'");
+                newBook.ISBN = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Who is the Author?");
+                newBook.Author = Console.ReadLine();
+                Console.WriteLine("What is your review of the book? If you have not read it or do not want to write a review, say 'N/A'.");
+                newBook.Review = Console.ReadLine();
+                return newBook;
         }
     }
 }
